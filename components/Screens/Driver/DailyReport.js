ebb3fc56ad8b333d16f0d162fd3c0944e70c008f
SamalDulki <samalya.20230321@iit.ac.lk>
2025-06-22 21:10:55 +0530

    Daily Report adding

---
+++ components/Screens/Driver/DailyReport.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Alert,
+} from "react-native";
+
+export default function DailyReport() {
+  const [date, setDate] = useState("");
+  const [area, setArea] = useState("");
+  const [description, setDescription] = useState("");
+}
